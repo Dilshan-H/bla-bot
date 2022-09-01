b57fa59a70cf46e7df4fe9884051a54489025a82
dilshan-h <77499497+Dilshan-H@users.noreply.github.com>
2022-09-01 11:54:08 +0530

    Update gitignore and fix version

--- bla_bot.py
+++ bla_bot.py
@@ -75,7 +75,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # BOT INFO
-BOT_VERSION: str = "1.6.0"
+BOT_VERSION: str = "1.7.1"
 BOT_NAME: str = "TEMP BOT"
 BOT_DESCRIPTION: str = """Born on: 2022.08.20 in Sri Lanka.\n
 And, Hey, I'm an open-source bot written in Python.
